dbecfd7e1bfa3d4e5552e385592e2dfd81cf15d6
Jannis Baum <jannis.baum@gmail.com>
2024-04-22 17:51:59 +0200

    feat: output organization

--- data_collection.py
+++ data_collection.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+import os
 
 import cv2
 import pandas as pd
@@ -7,17 +8,22 @@ from helpers import millis
 from myo import setup_myo, stop_myo
 
 if __name__ == '__main__':
+    # argument parsing & outputs
     parser = ArgumentParser()
     parser.add_argument('output', type=str)
     args = parser.parse_args()
+    os.mkdir(args.output)
+    def path(component: str):
+        return os.path.join(args.output, component)
 
+    # myo setup
     queue = setup_myo()
-
     df = pd.DataFrame(columns=['millis', *[f'EMG{i}' for i in range(8)]])
 
+    # camera/video setup
     size = (480, 270)
     cap = cv2.VideoCapture(0)
-    writer = cv2.VideoWriter(f'{args.output}.mp4', cv2.VideoWriter_fourcc(*'AVC1'), 20, size)
+    writer = cv2.VideoWriter(path('video.mp4'), cv2.VideoWriter_fourcc(*'AVC1'), 20, size)
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -38,10 +44,13 @@ if __name__ == '__main__':
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+    # myo teardown
     stop_myo()
+    df.to_csv(path('data.csv'), index=False)
+
+    # camera/video teardown
     cap.release()
     writer.release()
     cv2.destroyAllWindows()
 
-    df.to_csv(f'{args.output}.csv', index=False)
     quit()
