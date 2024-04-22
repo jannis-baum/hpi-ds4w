20ed0332ba92c3c0d92614d6c820beff6377a8b4
Jannis Baum <jannis.baum@gmail.com>
2024-04-22 18:28:26 +0200

    feat: plotting area

--- data_collection.py
+++ data_collection.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 import os
 
 from cv2.typing import MatLike
+import numpy as np
 import pandas as pd
 
 from helpers import millis
@@ -21,15 +22,24 @@ if __name__ == '__main__':
     queue = setup_myo()
     df = pd.DataFrame(columns=['millis', *[f'EMG{i}' for i in range(8)]])
 
+    # plotting setup
+    size = (480, 270)
+    plot = np.zeros((*size[::-1], 3), np.uint8)
+    step = 3
+
     def frame_handler(frame: MatLike) -> MatLike:
+        global plot
+        plot = np.roll(plot, -step, axis=1)
+        plot[:, -step:, :] = 255
+
         while not(queue.empty()):
             emg = list(queue.get())
             time = millis()
             df.loc[len(df)] = [time, *emg]
             print(('{:7}: ' + ' | '.join(['{:4}'] * 8)).format(time, *emg))
-        return frame
+        return np.concatenate((plot, frame), axis=0)
 
-    rec = VideoRecorder(frame_handler, path('video.mp4'))
+    rec = VideoRecorder(frame_handler, path('video.mp4'), resize=size, out_size=(size[0], size[1] * 2))
     rec.run()
 
     # myo teardown
--- video.py
+++ video.py
@@ -9,13 +9,14 @@ class VideoRecorder:
         self,
         loop_callback: Callable[[MatLike], MatLike],
         save_path: str | None = None,
-        size: tuple[int, int] = (480, 270)
+        resize: tuple[int, int] = (480, 270),
+        out_size: tuple[int, int] = (480, 270)
     ):
-        self.size = size
+        self.resize = resize
         self.loop_callback = loop_callback
         self.cap = cv2.VideoCapture(0)
         if save_path:
-            self.writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'AVC1'), 20, size)
+            self.writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'AVC1'), 20, out_size)
         else:
             self.writer = None
 
@@ -26,7 +27,7 @@ class VideoRecorder:
                  print("Can't receive frame (stream end?). Exiting ...")
                  break
 
-            frame = cv2.resize(cv2.flip(frame, 1), self.size)
+            frame = cv2.resize(cv2.flip(frame, 1), self.resize)
             frame = self.loop_callback(frame)
 
             if self.writer:
