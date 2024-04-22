614bed759497f9e52840e63c59b748e04cdd2360
Jannis Baum <jannis.baum@gmail.com>
2024-04-22 18:04:03 +0200

    refactor: video recorder own file

--- data_collection.py
+++ data_collection.py
@@ -1,11 +1,12 @@
 from argparse import ArgumentParser
 import os
 
-import cv2
+from cv2.typing import MatLike
 import pandas as pd
 
 from helpers import millis
 from myo import setup_myo, stop_myo
+from video import VideoRecorder
 
 if __name__ == '__main__':
     # argument parsing & outputs
@@ -20,37 +21,17 @@ if __name__ == '__main__':
     queue = setup_myo()
     df = pd.DataFrame(columns=['millis', *[f'EMG{i}' for i in range(8)]])
 
-    # camera/video setup
-    size = (480, 270)
-    cap = cv2.VideoCapture(0)
-    writer = cv2.VideoWriter(path('video.mp4'), cv2.VideoWriter_fourcc(*'AVC1'), 20, size)
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-             print("Can't receive frame (stream end?). Exiting ...")
-             break
-        frame = cv2.resize(cv2.flip(frame, 1), size)
-        
-        writer.write(frame)
-        cv2.imshow('frame', frame)
-
+    def frame_handler(frame: MatLike) -> MatLike:
         while not(queue.empty()):
             emg = list(queue.get())
             time = millis()
             df.loc[len(df)] = [time, *emg]
             print(('{:7}: ' + ' | '.join(['{:4}'] * 8)).format(time, *emg))
+        return frame
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    rec = VideoRecorder(frame_handler, path('video.mp4'))
+    rec.run()
 
     # myo teardown
     stop_myo()
     df.to_csv(path('data.csv'), index=False)
-
-    # camera/video teardown
-    cap.release()
-    writer.release()
-    cv2.destroyAllWindows()
-
-    quit()
---
+++ video.py
@@ -0,0 +1,42 @@
+from typing import Callable
+
+import cv2
+from cv2.typing import MatLike
+
+
+class VideoRecorder:
+    def __init__(
+        self,
+        loop_callback: Callable[[MatLike], MatLike],
+        save_path: str | None = None,
+        size: tuple[int, int] = (480, 270)
+    ):
+        self.size = size
+        self.loop_callback = loop_callback
+        self.cap = cv2.VideoCapture(0)
+        if save_path:
+            self.writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'AVC1'), 20, size)
+        else:
+            self.writer = None
+
+    def run(self):
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if not ret:
+                 print("Can't receive frame (stream end?). Exiting ...")
+                 break
+
+            frame = cv2.resize(cv2.flip(frame, 1), self.size)
+            frame = self.loop_callback(frame)
+
+            if self.writer:
+                self.writer.write(frame)
+            cv2.imshow('frame', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        self.cap.release()
+        if self.writer:
+            self.writer.release()
+        cv2.destroyAllWindows()
