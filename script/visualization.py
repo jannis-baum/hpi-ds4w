47a5b474305e173477295156a966f134183b7ad5
Jannis Baum <jannis.baum@gmail.com>
2024-05-18 09:48:06 +0200

    feat: pretty heatmap

--- script/visualization.py
+++ script/visualization.py
@@ -1,8 +1,11 @@
 from IPython.display import Markdown, display
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.metrics import accuracy_score
 
+# confusion matrix
 def report_cm(title, y_true, y_pred, classifier):
     display(Markdown(f'# {title}'))
     print(f'accuracy: {accuracy_score(y_true, y_pred)}')
@@ -13,3 +16,23 @@ def report_cm(title, y_true, y_pred, classifier):
         xticks_rotation='vertical'
     )
     plt.show()
+
+# pretty heatmap
+# by default set to plot -log(p-value), adjust transform/legend args if using
+# for something else
+def pretty_hm(
+    data, labels,
+    transform = lambda x: -np.log(x + np.nextafter(0, 1)),
+    legend='-log(p-value)', annot = True, cmap = 'flare'
+):
+    up_triang = np.triu(np.ones_like(data)).astype(bool)
+    ax = sns.heatmap(
+        transform(data) if transform else data,
+        cmap=cmap, xticklabels=False, yticklabels=False,
+        annot=annot, fmt='.0f',
+        square=True, linecolor='white', linewidths=0.5,
+        cbar=True, mask=up_triang, cbar_kws={'shrink': 0.6, 'pad': 0.02, 'label': legend}
+    )
+    ax.invert_xaxis()
+    for i, label in enumerate(labels):
+        ax.text(i + 0.2, i + 0.5, label, ha='right', va='center')
