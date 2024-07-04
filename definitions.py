1e91861be419a436b3aad6cc6841cfbf57b9a697
Jannis Baum <jannis.baum@gmail.com>
2024-07-04 10:41:40 +0200

    chore: remove old features.json

--- definitions.py
+++ definitions.py
@@ -1,4 +1,3 @@
 import os
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
-features_path = os.path.join(root_dir, 'features.json')
--- script/dataset.py
+++ script/dataset.py
@@ -28,7 +28,3 @@ def get_data() -> DataSet:
 # extract information from id column
 def get_id_col(X: pd.DataFrame, col) -> pd.Series:
     return X['id'].str.split('_', expand=True)[col]
-
-def get_feature_list():
-    with open(features_path, 'r') as fp:
-        return json.load(fp)
