37a1d33604e3e5f259a7cc9a0d9cc6fd8750cc42
NikolaiRumiantsev <nikrum.hb@gmail.com>
2024-07-10 15:10:10 +0200

    #1 completed

--- script/dataset.py
+++ script/dataset.py
@@ -1,7 +1,5 @@
 import os
-
 import pandas as pd
-
 from definitions import data_dir
 
 dataset_path = os.path.join(data_dir, 'dataset.csv')
@@ -16,11 +14,21 @@ cols_emg_cal = [f'{col}_cal' for col in cols_emg]
 # X (id, time, EMG data), y (hold labels)
 DataSet = tuple[pd.DataFrame, pd.Series]
 
-def get_data() -> DataSet:
+def get_data(simplify: bool = True) -> DataSet:
     dataset = pd.read_csv(dataset_path)
     X = pd.DataFrame(dataset[[col_id, col_time, *cols_emg, *cols_emg_cal]])
     y = pd.Series(dataset[col_hold]).set_axis(dataset[col_id])
     y = pd.Series(y[~y.index.duplicated(keep='first')])
+
+    if simplify:
+        hold_mapping = {
+            'crimp_45': 'crimp',
+            'crimp_20': 'crimp',
+            'jug': 'sloper_jug',
+            'sloper_30': 'sloper_jug'
+        }
+        y = y.map(hold_mapping)
+
     return (X, y)
 
 # extract information from id column
