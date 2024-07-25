c26d7c396cf54baa081da184b74c926783c9c03a
Jannis Baum <jannis.baum@gmail.com>
2024-07-25 20:54:51 +0200

    feat(#3): make SplitDataset work with numpy arrays

--- script/dataset.py
+++ script/dataset.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy.typing as npt
 import pandas as pd
 from sklearn.model_selection import KFold
 
@@ -42,26 +43,33 @@ def get_id_col(X: pd.DataFrame, col) -> pd.Series:
 class SplitDataset:
     def __init__(
         self,
-        data: pd.DataFrame, labels: pd.Series,
+        # data and labels should both either be pandas or numpy
+        data: pd.DataFrame | npt.NDArray, labels: pd.Series | npt.NDArray,
         participants: pd.Series, test_p: str = 'p1',
         n_folds: int = 5, random_state: int = 42
     ):
         split = participants == test_p
 
-        self._data_train, self._data_test = pd.DataFrame(data[~split]), pd.DataFrame(data[split])
-        self._labels_train, self._labels_test = pd.Series(labels[~split]), pd.Series(labels[split])
+        self._data_train, self._data_test = data[~split], data[split]
+        self._labels_train, self._labels_test = labels[~split], labels[split]
 
         self.n_folds = n_folds
         kf = KFold(n_splits=self.n_folds, random_state=random_state, shuffle=True)
         self._folds = list(kf.split(self._data_train))
 
+    # get data from pandas DataFrame or Series or numpy array
+    def _get_data(self, data: pd.DataFrame | pd.Series | npt.NDArray, idx):
+        if isinstance(data, pd.DataFrame) or isinstance(data, pd.Series):
+            return data.iloc[idx]
+        return data[idx]
+
     # get folded training data
-    def get_train(self) -> list[DataSet]:
+    def get_train(self) -> list[tuple]:
         return [
             (lambda idx_train:
-                 (self._data_train.iloc[idx_train], self._labels_train.iloc[idx_train])
+                (self._get_data(self._data_train, idx_train), self._get_data(self._labels_train, idx_train))
             )(self._folds[fold][0])
         for fold in range(self.n_folds)]
 
-    def get_test(self) -> DataSet:
+    def get_test(self) -> tuple:
         return (self._data_test, self._labels_test)
