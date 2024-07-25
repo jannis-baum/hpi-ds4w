ca55d906b0f5ae7fcefff61a69f52776700c2d1b
Jannis Baum <jannis.baum@gmail.com>
2024-07-25 17:40:42 +0200

    feat(#3): split dataset

--- script/dataset.py
+++ script/dataset.py
@@ -1,6 +1,7 @@
 import os
 
 import pandas as pd
+from sklearn.model_selection import KFold
 
 from definitions import data_dir
 
@@ -36,3 +37,31 @@ def get_data(simplify: bool = True) -> DataSet:
 # extract information from id column
 def get_id_col(X: pd.DataFrame, col) -> pd.Series:
     return X['id'].str.split('_', expand=True)[col]
+
+# split data into train/test and create folds for tests
+class SplitDataset:
+    def __init__(
+        self,
+        data: pd.DataFrame, labels: pd.Series,
+        participants: pd.Series, test_p: str = 'p1',
+        n_folds: int = 5, random_state: int = 42
+    ):
+        split = participants == test_p
+
+        self._data_train, self._data_test = pd.DataFrame(data[~split]), pd.DataFrame(data[split])
+        self._labels_train, self._labels_test = pd.Series(labels[~split]), pd.Series(labels[split])
+
+        self.n_folds = n_folds
+        kf = KFold(n_splits=self.n_folds, random_state=random_state, shuffle=True)
+        self._folds = list(kf.split(self._data_train))
+
+    # get folded training data
+    def get_train(self) -> list[DataSet]:
+        return [
+            (lambda idx_train:
+                 (self._data_train.iloc[idx_train], self._labels_train.iloc[idx_train])
+            )(self._folds[fold][0])
+        for fold in range(self.n_folds)]
+
+    def get_test(self) -> DataSet:
+        return (self._data_test, self._labels_test)
